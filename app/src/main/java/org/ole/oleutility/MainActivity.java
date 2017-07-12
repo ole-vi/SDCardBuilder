17e829a73158f62b0a38307b5d00394c8da7cf29
Kyle Mathias <kylemathias@gmail.com>
2017-07-12 13:50:01 -0700

    First upload

---
+++ app/src/main/java/org/ole/oleutility/MainActivity.java
@@ -0,0 +1,55 @@
+package org.ole.oleutility;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void wipeMemoryCard() {
+        File deleteMatchingFile = new File(Environment
+                .getExternalStorageDirectory().toString());
+        try {
+            File[] filenames = deleteMatchingFile.listFiles();
+            if (filenames != null && filenames.length > 0) {
+                for (File tempFile : filenames) {
+                    if (tempFile.isDirectory()) {
+                        wipeDirectory(tempFile.toString());
+                        tempFile.delete();
+                    } else {
+                        tempFile.delete();
+                    }
+                }
+            } else {
+                deleteMatchingFile.delete();
+            }
+        } catch (Exception e) {
+            //Utils.log(e.getMessage());
+        }
+    }
+
+    private static void wipeDirectory(String name) {
+        File directoryFile = new File(name);
+        File[] filenames = directoryFile.listFiles();
+        if (filenames != null && filenames.length > 0) {
+            for (File tempFile : filenames) {
+                if (tempFile.isDirectory()) {
+                    wipeDirectory(tempFile.toString());
+                    tempFile.delete();
+                } else {
+                    tempFile.delete();
+                }
+            }
+        } else {
+            directoryFile.delete();
+        }
+    }
+
+}
