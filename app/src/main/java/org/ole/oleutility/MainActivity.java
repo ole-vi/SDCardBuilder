b2a95cb84fca10ee8b8a2b064cd5e1f56c08c0a8
Kyle Mathias <kylemathias@gmail.com>
2017-08-03 09:17:55 -0700

    Testing Changes

--- app/src/main/java/org/ole/oleutility/MainActivity.java
+++ app/src/main/java/org/ole/oleutility/MainActivity.java
@@ -3,53 +3,53 @@ package org.ole.oleutility;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void wipeMemoryCard() {
-        File deleteMatchingFile = new File(Environment
-                .getExternalStorageDirectory().toString());
+    private void copyFile(String inputPath, String inputFile, String outputPath) {
+
+        InputStream in = null;
+        OutputStream out = null;
         try {
-            File[] filenames = deleteMatchingFile.listFiles();
-            if (filenames != null && filenames.length > 0) {
-                for (File tempFile : filenames) {
-                    if (tempFile.isDirectory()) {
-                        wipeDirectory(tempFile.toString());
-                        tempFile.delete();
-                    } else {
-                        tempFile.delete();
-                    }
-                }
-            } else {
-                deleteMatchingFile.delete();
-            }
-        } catch (Exception e) {
-            //Utils.log(e.getMessage());
-        }
-    }
 
-    private static void wipeDirectory(String name) {
-        File directoryFile = new File(name);
-        File[] filenames = directoryFile.listFiles();
-        if (filenames != null && filenames.length > 0) {
-            for (File tempFile : filenames) {
-                if (tempFile.isDirectory()) {
-                    wipeDirectory(tempFile.toString());
-                    tempFile.delete();
-                } else {
-                    tempFile.delete();
-                }
+            //create output directory if it doesn't exist
+            File dir = new File (outputPath);
+            if (!dir.exists())
+            {
+                dir.mkdirs();
             }
-        } else {
-            directoryFile.delete();
-        }
-    }
 
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;
+
+        }  catch (FileNotFoundException fnfe1) {
+            Log.e("tag", fnfe1.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
 }
